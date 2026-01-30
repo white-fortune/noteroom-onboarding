37de29f87994b2ff78a0bf08fa958e94c7af0a4c
white-fortune <whitefortune271828@gmail.com>
2026-01-30 21:45:28 +0600

    middleware for password-reset and email-verification token checking

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl
+
+    if (pathname === "/password-reset/otp" || pathname === "/password-reset/reset") {
+        const passwordResetCookie = request.cookies.get("password-reset")
+        if (!passwordResetCookie) {
+            return NextResponse.redirect(new URL("/password-reset", request.url))
+        }
+    } else if (pathname === "/verify/email") {
+        const emailVerificationCookie = request.cookies.get("email-verification")
+        if (!emailVerificationCookie) {
+            return NextResponse.redirect(new URL("/signin", request.url))
+        }
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/password-reset/reset", "/password-reset/otp", "/verify/email"]
+};
