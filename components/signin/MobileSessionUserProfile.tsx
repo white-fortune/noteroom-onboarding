642e43abfd11e9e88433c49a96df3c6689c36498
white-fortune <whitefortune271828@gmail.com>
2026-02-08 21:02:38 +0600

    resolved the 'or' label div in contiue as mobile page

--- components/signin/MobileSessionUserProfile.tsx
+++ components/signin/MobileSessionUserProfile.tsx
@@ -9,7 +9,7 @@ export default function MobileSessionUserProfile({ user, setShowSessionUser }: {
     const router = useRouter()
 
     return (
-        <div className="w-full flex flex-col items-center px-6 gap-4 mt-10">
+        <div className="w-full flex flex-col items-center gap-4 mt-10">
             <div className="session-user w-fill flex flex-col items-center gap-3">
                 <div className="profile-image overflow-hidden rounded-full w-40 h-40">
                     <img
@@ -24,22 +24,27 @@ export default function MobileSessionUserProfile({ user, setShowSessionUser }: {
                 </div>
             </div>
 
-            <AuthButton label={`Continue as ${user.name}`} onClick={() => router.push("https://app.noteroom.co")} />
-
-            <div className="w-full flex items-center justify-center my-5 gap-2">
-                <div className="flex-1 h-px bg-slate-200"></div>
-                <div className="text-slate-500 text-xs font-semibold uppercase tracking-wide">
-                    or
-                </div>
-                <div className="flex-1 h-px bg-slate-200"></div>
+            <div className="w-full px-6 flex justify-center">
+                <AuthButton label={`Continue as ${user.name}`} onClick={() => router.push("https://app.noteroom.co")} />
             </div>
 
-            <button
-                onClick={() => setShowSessionUser(false)}
-                className="w-full max-w-md h-12 bg-white rounded-lg outline-1 -outline-offset-1 outline-sky-500 flex items-center justify-center text-sky-500 text-base font-medium hover:bg-sky-50 transition-all active:scale-[0.98]"
-            >
-                Continue to Sign In
-            </button>
+            <div className="w-full max-w-md h-6 relative flex justify-center items-center -mx-6">
+                <div className="w-full h-px bg-slate-200 absolute"></div>
+                <div className="bg-white px-3.5 py-1 rounded-3xl relative z-10">
+                    <span className="text-slate-500 text-[10px] font-semibold uppercase leading-4 tracking-wider">
+                        or
+                    </span>
+                </div>
+            </div>
+
+            <div className="w-full px-6 flex justify-center">
+                <button
+                    onClick={() => setShowSessionUser(false)}
+                    className="w-full max-w-md h-12 bg-white rounded-lg outline-1 -outline-offset-1 outline-sky-500 flex items-center justify-center text-sky-500 text-base font-medium hover:bg-sky-50 transition-all active:scale-[0.98]"
+                >
+                    Continue to Sign In
+                </button>
+            </div>
 
             <div className="w-full flex flex-col items-center mt-20">
                 <div className="flex flex-col items-center gap-1 mb-4">
